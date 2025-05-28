254d4bac9f5910a93b3fabd4f6a223c46a2d0add
Felipe <felip.goncalves@hotmail.com>
2025-05-27 21:05:52 -0300

    Verificando Autorização

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
+import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization';
 import { DeliveriesController } from '@/controllers/deliveries-controller';
-
 import { ensureAuthenticated } from '@/middlewares/ensure-authenticated';
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(['sale']));
 deliveriesRoutes.post('/', deliveriesController.create);
 
 export { deliveriesRoutes };
